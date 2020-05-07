87a6c3918e76f4a9fcd0483c6572e83fc39feb06
Andrew Park <parkandrewkh@gmail.com>
2020-05-06 22:44:23 -0700

    Updates

---
+++ precache-manifest.549394de9faf33b835e5943a18b13898.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7afae59d2077ea8846636aaa7307bb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50c25353b2fb7d4fc7e4",
+    "url": "/static/css/main.c33d11ce.chunk.css"
+  },
+  {
+    "revision": "f2477949b597b6440bbf",
+    "url": "/static/js/2.a636507f.chunk.js"
+  },
+  {
+    "revision": "50c25353b2fb7d4fc7e4",
+    "url": "/static/js/main.22e367ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
