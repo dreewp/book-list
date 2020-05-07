f24bea8c1b4fa406f25ed4a6bca1df36a81f9658
Andrew Park <parkandrewkh@gmail.com>
2020-05-06 23:34:42 -0700

    Updates

---
+++ precache-manifest.a9a085075c769887f8efd5b1fb2cf4fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b600ae02ee7495ed4efe25df58af116",
+    "url": "/book-list/index.html"
+  },
+  {
+    "revision": "5cfb59738ff997d9ec2b",
+    "url": "/book-list/static/css/main.2e081780.chunk.css"
+  },
+  {
+    "revision": "5ab02479df3fe30b87a4",
+    "url": "/book-list/static/js/2.8c0e5d29.chunk.js"
+  },
+  {
+    "revision": "5cfb59738ff997d9ec2b",
+    "url": "/book-list/static/js/main.bdd11ed1.chunk.js"
+  },
+  {
+    "revision": "370557dc34b895d25c41",
+    "url": "/book-list/static/js/runtime~main.88ddd283.js"
+  },
+  {
+    "revision": "22f0f6831f47cdadd81f3af1fd418747",
+    "url": "/book-list/static/media/wood-background1.22f0f683.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b76496850806a554d8eb4141e450a052.js"
+  "/book-list/precache-manifest.a9a085075c769887f8efd5b1fb2cf4fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/book-list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
